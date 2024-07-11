05483bb328c87dcfd0a5b4190da6eef5dc95d21d
YoungChulShin <go1323@gmail.com>
2024-07-11 23:37:56 +0900

    프로젝트 초안

---
+++ hellospring/src/test/java/study/spring/hellospring/HellospringApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
