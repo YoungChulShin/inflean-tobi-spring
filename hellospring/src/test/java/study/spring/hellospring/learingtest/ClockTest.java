506612792a3115a35a884429ab67daa34766e919
YoungChulShin <go1323@gmail.com>
2024-07-19 00:00:32 +0900

    Clock을 이용한 고정 시간 테스트

--- hellospring/src/test/java/study/spring/hellospring/learingtest/ClockTest.java
+++ hellospring/src/test/java/study/spring/hellospring/learingtest/ClockTest.java
@@ -1,7 +1,9 @@
 package study.spring.hellospring.learingtest;
 
 import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +11,14 @@ public class ClockTest {
 
   @Test
   void clock() {
-    Clock clock = Clock.systemDefaultZone();
+    Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
 
     LocalDateTime dt1 = LocalDateTime.now(clock);
     LocalDateTime dt2 = LocalDateTime.now(clock);
+    LocalDateTime dt3 = LocalDateTime.now(clock).plusHours(1);
 
-    Assertions.assertThat(dt2).isAfter(dt1);
+    Assertions.assertThat(dt2).isEqualTo(dt1);
+    Assertions.assertThat(dt3).isEqualTo(dt1.plusHours(1));
   }
 
   // clock을 이용해서 localDatetime.now
