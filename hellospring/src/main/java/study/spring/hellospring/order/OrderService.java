e38d16ef729e0086ab68b7766220def88882abde
YoungChulShin <go1323@gmail.com>
2024-07-29 00:02:10 +0900

    OrderService 추가

---
+++ hellospring/src/main/java/study/spring/hellospring/order/OrderService.java
@@ -0,0 +1,29 @@
+package study.spring.hellospring.order;
+
+import java.math.BigDecimal;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionTemplate;
+import study.spring.hellospring.data.OrderRepository;
+
+@Service
+public class OrderService {
+
+  private final OrderRepository orderRepository;
+  private final JpaTransactionManager transactionManager;
+
+  public OrderService(OrderRepository orderRepository, JpaTransactionManager transactionManager) {
+    this.orderRepository = orderRepository;
+    this.transactionManager = transactionManager;
+  }
+
+
+  public Order createOrder(String no, BigDecimal total) {
+    Order order = new Order(no, total);
+
+    return new TransactionTemplate(transactionManager).execute(status -> {
+      orderRepository.save(order);
+      return order;
+    });
+  }
+}
